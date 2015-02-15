62169c8b413852756bccbefc913f15ac6d9ee0e7
Inaba Byakko <hct.inb.1501.yk@gmail.com>
2015-02-15 16:27:45 +0900

    ユーザーの全戦績取得

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -1,11 +1,11 @@
 class RecordController < ApplicationController
   def user
-		uid = TwitterUser.select(:id).where(screen_name:  params[:screen_name])
+		@rounds = []
+		uids = TwitterUser.select(:id).where(screen_name:  params[:screen_name]).to_a
 		
 		# 指定したユーザーの半荘IDを取得
-		round_id_list =UserRoundResults.where(user_id: uid).select(:round_id)
+		round_id_list =UserRoundResults.where("user_id in ?", uids).select(:round_id)
 		
-		@rounds = []
 		round_id_list.each do |rid|
 			@rounds.push( UserRoundResults.where(:round_id => rid).order("seat ASC").to_a)
 		end
