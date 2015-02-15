7fab8badcba50bab497cb81e31ed94d41bcc8bab
Inaba Byakko <hct.inb.1501.yk@gmail.com>
2015-02-15 16:08:08 +0900

    ユーザーの全戦績取得

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -1,4 +1,11 @@
 class RecordController < ApplicationController
   def user
+		# 指定したユーザーの半荘IDを取得
+		round_id_list =UserRoundResults.where(screen_name: params[:screen_name]).select(:round_id)
+		
+		@rounds = []
+		round_id_list.each do |rid|
+			@rounds.push( UserRoundResults.where(:round_id => rid).order("seat ASC").to_a)
+		end
   end
 end
