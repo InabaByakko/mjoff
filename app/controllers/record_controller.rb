b33b51befc94dabf00c1c9368886c688289e30fb
Inaba Byakko <hct.inb.1501.yk@gmail.com>
2015-02-15 16:45:05 +0900

    ユーザーの全戦績取得

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -3,6 +3,7 @@ class RecordController < ApplicationController
 		@users = TwitterUser.all.to_a
 		@rounds = []
 		uids = TwitterUser.select(:id).where(screen_name:  params[:screen_name]).to_a
+		# TODO: 存在しないscreen_nameを指定したときエラー画面
 		
 		# 指定したユーザーの半荘IDを取得
 		round_id_list =UserRoundResults.where("user_id IN (?)", uids).select(:round_id)
